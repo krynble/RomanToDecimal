efcd341d50a71135f467f9d4231825685166fec6
Omar Ajoue <omar.ajoue@gmail.com>
2013-08-01 21:44:29 -0300

    Improved the calculator by adding the substraction cases

--- server.js
+++ server.js
@@ -1,6 +1,46 @@
+/** 
+ * Takes a Roman numeral string and returns it's decimal value
+ * @param  string  romanString The Roman Numeral input
+ * @return integer             the decimal value or -1 in case of a malformatted string
+ */
 function romanToDecimal(romanString) {
 	var output = 0, i;
 	romanString = romanString.toUpperCase();
+
+	/* Check for a few special cases - substractions. */
+
+	// IV = 4
+	i = romanString.match(/IV/g);
+	output += ( i ? i.length : 0 ) * 4;
+	romanString = romanString.replace(/IV/g, '');
+
+	// IX = 9
+	i = romanString.match(/IX/g);
+	output += ( i ? i.length : 0 ) * 9;
+	romanString = romanString.replace(/IX/g, '');
+
+	// XL = 40
+	i = romanString.match(/XL/g);
+	output += ( i ? i.length : 0 ) * 40;
+	romanString = romanString.replace(/XL/g, '');
+
+	// XC = 90
+	i = romanString.match(/XC/g);
+	output += ( i ? i.length : 0 ) * 90;
+	romanString = romanString.replace(/XC/g, '');
+
+	// CD = 400
+	i = romanString.match(/CD/g);
+	output += ( i ? i.length : 0 ) * 400;
+	romanString = romanString.replace(/CD/g, '');
+
+	// CM = 900
+	i = romanString.match(/CM/g);
+	output += ( i ? i.length : 0 ) * 900;
+	romanString = romanString.replace(/CM/g, '');
+	
+
+	/* Calculate standard cases */
 	for (i = 0; i < romanString.length; i++) {
 		switch(romanString[i]) {
 			case 'M':
@@ -32,6 +72,9 @@ function romanToDecimal(romanString) {
 	return output;
 }
 
+console.log('Output for iv: ' + romanToDecimal('iv'));
+console.log('Output for cm: ' + romanToDecimal('cm'));
+console.log('Output for mc: ' + romanToDecimal('mc'));
 console.log('Output for xlim: ' + romanToDecimal('xlim'));
 console.log('Output for mcvi: ' + romanToDecimal('mcvi'));
 console.log('Output for abcd: ' + romanToDecimal('abcd'));
